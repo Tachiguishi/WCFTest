d6d1d692637301f780ca09e35aed650837a356aa
TachiTukimi <TachiguishiRor@gmail.com>
2016-06-01 11:44:48 +0800

    Hello world

---
+++ WCFServicePilot101/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace WCFServicePilot101
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public int ID {get;set;}
+
+        [DataMember]
+        public string Name {get;set;}
+    }
+}
---
+++ WCFServicePilot101/User.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Activation;
+
+namespace WCFServicePilot101
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“User”
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class User : IUser
+    {
+        public string ShowName(string name)
+        {
+            string wcfName = string.Format("WCF服务，显示姓名：{0}", name);
+            return wcfName;
+        }
+
+        public Person GetOnePerson()
+        {
+            return new Person {ID = 1, Name = "goDie"};
+        }
+
+        public List<Person> GetPersons(int id, string name)
+        {
+            return new List<Person>(){new Person{ID = 1, Name = "Nux"},
+                   new Person{ID = 2, Name = "Furiosa"}};
+        }
+    }
+}
